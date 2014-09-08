314cf61c17986ca3f9befbfadb1bf62e28670f79
Danny Hurtado <dannylhurtado@gmail.com>
2014-09-08 12:52:56 -0500

    Paquete del mundo y su clase del mundo

---
+++ src/com/moviles/mundo/HApper.java
@@ -0,0 +1,24 @@
+package com.moviles.mundo;
+
+public class HApper 
+{
+	/**
+	 * Instancia del mundo
+	 */
+	private HApper instancia;
+	
+	/**
+	 * Constructor del mundo que inicializa 
+	 */
+	public HApper() 
+	{
+		super();
+	}
+	
+	public HApper darInstancia()
+	{
+		if(instancia == null)
+			instancia = new HApper();
+		return instancia;
+	}
+}
