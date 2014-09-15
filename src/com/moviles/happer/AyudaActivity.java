c600ea934c261587419f27514a12e671b70cde65
Santiago Arias <sad.arias.d@gmail.com>
2014-09-15 15:04:14 -0500

    algo con lo q perdi el tiempo

--- src/com/moviles/happer/AyudaActivity.java
+++ src/com/moviles/happer/AyudaActivity.java
@@ -1,33 +1,23 @@
 package com.moviles.happer;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.moviles.mundo.HApper;
+public class AyudaActivity extends Activity {
 
-public class AjustesAlarmasActivity extends ActionBarActivity
-{
-	/**
-	 * Atributo que modela la instancia del mundo
-	 */
-	private HApper instancia;
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_ajustes_alarma);
-		instancia = HApper.darInstancia(getApplicationContext());
+		setContentView(R.layout.activity_ayuda);
 	}
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) 
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-				getMenuInflater().inflate(R.menu.ajustes_alarma, menu);
-				return true;
+		getMenuInflater().inflate(R.menu.ayuda, menu);
+		return true;
 	}
 
 	@Override
